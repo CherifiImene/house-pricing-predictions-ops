5f531596bdcaf35743ea935966f4736ad58fcf46
Cherifi Imane <hi_cherifi@esi.dz>
2023-03-05 10:28:43 +0000

    Added script to upload the container to docker hub

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=imenecherifi/pricespredictions
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+cat ~/my_password.txt | docker login --username imenecherifi --password-stdin
+docker tag pricespredictions imenecherifi/pricespredictions
 
 # Step 3:
 # Push image to a docker repository
+docker push imenecherifi/pricespredictions
