ecac5cd5a3c78f02fa15912eb501b249396f676d
Cherifi Imane <hi_cherifi@esi.dz>
2023-03-05 12:55:46 +0000

    Added Script to run container on kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-
+dockerpath=imenecherifi/pricespredictions
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run pricespredictions --image=$dockerpath --port=8000
 
 # Step 3:
 # List kubernetes pods
-
+kubectl get pod
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pricespredictions 8000:80
