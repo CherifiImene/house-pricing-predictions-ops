582913a3a9de341119f89c1b4c460af42020e792
Cherifi Imane <hi_cherifi@esi.dz>
2023-03-05 07:43:19 +0000

    Added commands to create docker container

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,11 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag=pricespredictions .
 
 # Step 2: 
 # List docker images
-
+docker image ls
 # Step 3: 
 # Run flask app
+docker run -p 0.0.0.0:80:8000/tcp -it pricespredictions bash
